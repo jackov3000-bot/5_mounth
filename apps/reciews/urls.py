b5d2243b2640cf63c9c475be2c20cda5f7f1ce2d
jack303 <jackov3000gmail.com>
2026-05-18 19:42:33 +0600

    Вот

--- apps/reciews/urls.py
+++ apps/reciews/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from apps.reciews.views import ReviewViewSet
 
 router = DefaultRouter()
-router.register('reviews', ReviewViewSet, basename='review')
+router.register('', ReviewViewSet, basename='review')
 
 urlpatterns = router.urls
--- apps/reciews/views.py
+++ apps/reciews/views.py
@@ -1,9 +1,11 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
 from apps.reciews.models import Review
 from apps.reciews.serializers import ReviewSerializer
 
-class ReviewViewSet(ModelViewSet):
+class ReviewViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-
+    
 # Create your views here.
+
